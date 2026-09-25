a9c70b287e7791a768943deb79369d1508b5aaff
GraciaRosalinaHindirwan <Rosalinagracia331@gmail.com>
2026-09-25 15:16:02 +0700

    finish UI login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,20 +8,126 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: 
-      Container(
-        child: Column(
-          children: [
-            Text(
-              "Login", 
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16, 
-                color: AppColors.textPrimary,
-                fontWeight: FontWeight.bold, 
-              ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(padding: EdgeInsetsGeometry.all(36),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "Login", 
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24, 
+                        color: AppColors.textPrimary,
+                        fontWeight: FontWeight.bold, 
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      "Hi Welcome Back, You've been missed", 
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16, 
+                        color: AppColors.textSecondary,
+                        fontWeight: FontWeight.normal, 
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 24),
+
+                Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        filled: true,
+                        fillColor: AppColors.cardBackground,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(
+                            color: AppColors.secondary, 
+                          ),
+                        ),
+
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(
+                            color: AppColors.primary,
+                            width: 2, 
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 16),
+
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        filled: true,
+                        fillColor: AppColors.cardBackground,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(
+                            color: AppColors.secondary, 
+                          ),
+                        ),
+
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(
+                            color: AppColors.primary,
+                            width: 2, 
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 24),
+
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: (){
+                      // aksi login disini
+                    }, 
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primary,
+                      foregroundColor: AppColors.white, 
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusGeometry.circular(12),
+                      ),
+                    ),
+
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700, 
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
